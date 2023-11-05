0c035130644e2f1c52a5bb1a4bf23770b1462995
Gia Henville <140769584+giabui@users.noreply.github.com>
2023-11-04 20:51:10 -0400

    feat: Init tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html"],
+
+  theme: {
+    extend: {
+      // ...
+    },
+  },
+  plugins: [],
+}
+
+
+
+
+// module.exports = {
+//   content: ["./index.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
