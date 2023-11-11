f05f45f52449b8b6dd56e656369ab466f9cf6c15
Gia Henville <140769584+giabui@users.noreply.github.com>
2023-11-10 21:12:53 -0500

    fix: Fix border colors
    Change border colors back to white when page reloads

--- script.js
+++ script.js
@@ -11,8 +11,25 @@ const passwordConfirm = () => {
 
     if (password === confirmPassword) {
         confirmPasswordInput.style.border = '1px solid rgb(34 197 94 / var(--tw-border-opacity))';
+        confirmPasswordInput.setCustomValidity(''); // Reset custom validity message
     } else {
         confirmPasswordInput.style.border = '1px solid rgb(239 68 68 / var(--tw-border-opacity))';
         confirmPasswordInput.setCustomValidity('Passwords do not match.')
     }
 }
+
+let formInputs = document.querySelectorAll('.rounded-sm');
+
+const borderColors = () => {
+    formInputs.forEach((input) => {
+        if (input.value === "") {
+            input.style.border = '1px solid #F5F5F5';
+        } else if (input.checkValidity()) {
+            input.style.border = '1px solid rgb(34 197 94 / var(--tw-border-opacity))';
+        } else {
+            input.style.border = '1px solid rgb(239 68 68 / var(--tw-border-opacity))';
+        }
+    });
+};
+
+borderColors();
