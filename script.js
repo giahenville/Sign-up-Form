30ff48c58860a4c66e686ad3a1eab0d8cc2a423b
Gia Henville <140769584+giabui@users.noreply.github.com>
2023-11-10 19:17:29 -0500

    feat: Add confirm password validation

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+//animation
 document.addEventListener("DOMContentLoaded", function () {
     const container = document.querySelector(".transform");
     container.classList.remove("translate-y-full");
 });
+
+const passwordConfirm = () => {
+    let password = document.getElementById('password').value;
+    let confirmPasswordInput = document.getElementById('password_confirm');
+    let confirmPassword = confirmPasswordInput.value;
+
+    if (password === confirmPassword) {
+        confirmPasswordInput.style.border = '1px solid rgb(34 197 94 / var(--tw-border-opacity))';
+    } else {
+        confirmPasswordInput.style.border = '1px solid rgb(239 68 68 / var(--tw-border-opacity))';
+        confirmPasswordInput.setCustomValidity('Passwords do not match.')
+    }
+}
